6f07725b6192121680503b12a81879578cbb4db5
Yan <mamul.93@mail.ru>
2022-12-22 23:34:12 +0900

    six example

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "марина")
+{
+    Console.WriteLine("Ура, это же МАРИНА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username); 
+}
